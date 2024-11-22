de16307476ef47a43093156ff2f65805dfea3aba
Skyler Mayfield <skyler544@gmail.com>
2024-11-22 14:33:39 +0100

    got the click handling working finally

--- drawing.js
+++ drawing.js
@@ -1,27 +1,88 @@
-class DrawingProgram {
-  constructor(canvas) {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext("2d");
-    this.setupEventListeners();
+application = () => {
+  class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
   }
 
-  setupEventListeners() {
-    document
-      .getElementById("undo")
-      .addEventListener("click", () => this.undo());
-    document
-      .getElementById("redo")
-      .addEventListener("click", () => this.redo());
+  class Line {
+    constructor(start, end) {
+      this.start = start;
+      this.end = end;
+    }
+
+    draw(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(this.start.x, this.start.y);
+      ctx.lineTo(this.end.x, this.end.y);
+      ctx.stroke();
+    }
   }
 
-  undo() {
-    console.log("FOO BAR BAZ");
+  class DrawingProgram {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = canvas.getContext("2d");
+      this.lines = [];
+      this.tempLineStart = null;
+      // TODO add undo / redo functionality
+
+      this.setupEventListeners();
+    }
+
+    setupEventListeners() {
+      this.canvas.addEventListener("click", (event) =>
+        this.handleLeftClick(event),
+      );
+
+      document
+        .getElementById("undo")
+        .addEventListener("click", () => this.undo());
+      document
+        .getElementById("redo")
+        .addEventListener("click", () => this.redo());
+    }
+
+    getMousePosition(event) {
+      const rect = this.canvas.getBoundingClientRect();
+      return new Point(event.clientX - rect.left, event.clientY - rect.top);
+    }
+
+    handleLeftClick(event) {
+      const point = this.getMousePosition(event);
+
+      console.log("FOO click!!!");
+      console.log(event);
+
+      if (this.tempLineStart === null) {
+        this.tempLineStart = point;
+      } else {
+        const line = new Line(this.tempLineStart, point);
+        this.lines.push(line);
+        // this.undoStack.push(line);
+        this.tempLineStart = null;
+        this.redraw();
+      }
+    }
+
+    redraw() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.lines.forEach((line) => line.draw(ctx));
+    }
+
+    undo() {
+      console.log("FOO BAR BAZ");
+    }
+
+    redo() {
+      console.log("BAR FOO");
+    }
   }
 
-  redo() {
-    console.log("BAR FOO");
-  }
-}
+  const canvas = document.getElementById("drawingCanvas");
+  new DrawingProgram(canvas);
+};
 
-const canvas = document.getElementById("drawingCanvas");
-new DrawingProgram(canvas);
+window.onLoad = application();
