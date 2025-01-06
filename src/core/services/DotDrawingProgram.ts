300778bac6c78b3ec6caf3280db1c76382b8dcfe
Skyler Mayfield <skyler544@gmail.com>
2025-01-06 20:08:07 +0100

    introduce redrawing function and undo stack

--- src/core/services/DotDrawingProgram.ts
+++ src/core/services/DotDrawingProgram.ts
@@ -5,17 +5,35 @@ export class DotDrawingProgram implements IDrawingProgram {
   private ctx: CanvasRenderingContext2D;
   private radius = 4;
 
+  private undoStack: Point[] = [];
+  private redoStack: Point[] = [];
+
   constructor(ctx: CanvasRenderingContext2D) {
     this.ctx = ctx;
   }
 
-  leftClick(point: Point) {
+  private redraw() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+    this.undoStack.forEach((point) => {
+      this.drawDot(point);
+    });
+  }
+
+  private drawDot(point: Point) {
     this.ctx.beginPath();
     this.ctx.arc(point.x, point.y, this.radius, 0, Math.PI * 2);
 
     this.ctx.fillStyle = "black";
     this.ctx.fill();
     this.ctx.closePath();
+  }
+
+  leftClick(point: Point) {
+    this.undoStack.push(point);
+    this.redoStack = [];
+
+    this.redraw();
   }
 
   doubleClick(): void {
